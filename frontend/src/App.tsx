d006cb3fa806b09c778dc87fcce40525faeb232d
sujalshah593 <sujalshah593@gmail.com>
2026-06-06 12:04:14 +0530

    Add folders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -47,8 +47,8 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+              <a href="https://react.dev"   target="_blank">
+                <img className="button-icon"  src={reactLogo} alt="" />
                 Learn more
               </a>
             </li>
